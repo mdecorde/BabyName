acfa4bc931cdcd6008d431a72c277e3c2e4f107a
Moritz Warning <moritzwarning@web.de>
2025-09-02 17:00:50 +0200

    rename app in license notices

--- app/src/main/kotlin/fr/hnit/babyname/MainActivity.kt
+++ app/src/main/kotlin/fr/hnit/babyname/MainActivity.kt
@@ -18,13 +18,13 @@ import fr.hnit.babyname.BabyNameDatabase.Companion.GENDER_FEMALE
 import fr.hnit.babyname.BabyNameDatabase.Companion.GENDER_MALE
 
 /*
-The babyname app is free software: you can redistribute it
+The Baby Name app is free software: you can redistribute it
 and/or modify it under the terms of the GNU General Public
 License as published by the Free Software Foundation,
 either version 2 of the License, or (at your option) any
 later version.
 
-The babyname app is distributed in the hope that it will be
+The Baby Name app is distributed in the hope that it will be
 useful, but WITHOUT ANY WARRANTY; without even the implied
 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE. See the GNU General Public License for more
