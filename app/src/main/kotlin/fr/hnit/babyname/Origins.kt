495a48bd9fc60d5e9575a40029f5bddad1bf136d
Moritz Warning <moritzwarning@web.de>
2025-10-09 17:35:37 +0200

    Origins: inline translation table
    This is better for the compiler to optimize.

--- app/src/main/kotlin/fr/hnit/babyname/Origins.kt
+++ app/src/main/kotlin/fr/hnit/babyname/Origins.kt
@@ -8,95 +8,95 @@ package fr.hnit.babyname
 import android.content.Context
 
 internal object Origins {
-    private val originMap = hashMapOf(
-        "african" to R.string.origin_item_african,
-        "albanian" to R.string.origin_item_albanian,
-        "ancientceltic" to R.string.origin_item_ancient_celtic,
-        "ancientegyptian" to R.string.origin_item_ancient_egyptian,
-        "ancientgermanic" to R.string.origin_item_ancient_germanic,
-        "ancientscandinavian" to R.string.origin_item_ancient_scandinavian,
-        "arabic" to R.string.origin_item_arabic,
-        "armenian" to R.string.origin_item_armenian,
-        "astronomy" to R.string.origin_item_astronomy,
-        "basque" to R.string.origin_item_basque,
-        "biblical" to R.string.origin_item_biblical,
-        "breton" to R.string.origin_item_breton,
-        "bulgarian" to R.string.origin_item_bulgarian,
-        "catalan" to R.string.origin_item_catalan,
-        "celtic" to R.string.origin_item_celtic,
-        "chinese" to R.string.origin_item_chinese,
-        "cornish" to R.string.origin_item_cornish,
-        "croatian" to R.string.origin_item_croatian,
-        "czech" to R.string.origin_item_czech,
-        "danish" to R.string.origin_item_danish,
-        "dutch" to R.string.origin_item_dutch,
-        "egyptian" to R.string.origin_item_egyptian,
-        "english" to R.string.origin_item_english,
-        "esperanto" to R.string.origin_item_esperanto,
-        "estonian" to R.string.origin_item_estonian,
-        "fareastern" to R.string.origin_item_far_eastern,
-        "finnish" to R.string.origin_item_finnish,
-        "french" to R.string.origin_item_french,
-        "frisian" to R.string.origin_item_frisian,
-        "galician" to R.string.origin_item_galician,
-        "german" to R.string.origin_item_german,
-        "germanic" to R.string.origin_item_germanic,
-        "greek" to R.string.origin_item_greek,
-        "hawaiian" to R.string.origin_item_hawaiian,
-        "hindu" to R.string.origin_item_hindu,
-        "history" to R.string.origin_item_history,
-        "hungarian" to R.string.origin_item_hungarian,
-        "icelandic" to R.string.origin_item_icelandic,
-        "indian" to R.string.origin_item_indian,
-        "iranian" to R.string.origin_item_iranian,
-        "irish" to R.string.origin_item_irish,
-        "italian" to R.string.origin_item_italian,
-        "japanese" to R.string.origin_item_japanese,
-        "jewish" to R.string.origin_item_jewish,
-        "judeo-christianlegend" to R.string.origin_item_juedeo_christian_legend,
-        "khmer" to R.string.origin_item_khmer,
-        "korean" to R.string.origin_item_korean,
-        "latvian" to R.string.origin_item_latvian,
-        "literature" to R.string.origin_item_literature,
-        "lithuanian" to R.string.origin_item_lithuanian,
-        "macedonian" to R.string.origin_item_macedonian,
-        "manx" to R.string.origin_item_manx,
-        "maori" to R.string.origin_item_maori,
-        "mormon" to R.string.origin_item_mormon,
-        "mythology" to R.string.origin_item_mythology,
-        "nativeamerican" to R.string.origin_item_native_american,
-        "neareastern" to R.string.origin_item_near_eastern,
-        "newworld" to R.string.origin_item_new_world,
-        "norse" to R.string.origin_item_norse,
-        "norwegian" to R.string.origin_item_norwegian,
-        "polish" to R.string.origin_item_polish,
-        "portuguese" to R.string.origin_item_portuguese,
-        "provençal" to R.string.origin_item_provençal,
-        "roman" to R.string.origin_item_roman,
-        "romanian" to R.string.origin_item_romanian,
-        "russian" to R.string.origin_item_russian,
-        "scandinavian" to R.string.origin_item_scandinavian,
-        "scottish" to R.string.origin_item_scottish,
-        "serbian" to R.string.origin_item_serbian,
-        "slavic" to R.string.origin_item_slavic,
-        "slovak" to R.string.origin_item_slovak,
-        "slovene" to R.string.origin_item_slovene,
-        "spanish" to R.string.origin_item_spanish,
-        "swedish" to R.string.origin_item_swedish,
-        "thai" to R.string.origin_item_thai,
-        "theology" to R.string.origin_item_theology,
-        "turkish" to R.string.origin_item_turkish,
-        "ukrainian" to R.string.origin_item_ukrainian,
-        "vietnamese" to R.string.origin_item_vietnamese,
-        "welsh" to R.string.origin_item_welsh
-    )
-
     fun getLocaleOrigin(ctx: Context, origin: String?): String {
         if (origin == null) {
             return ""
         }
 
-        val id = originMap[origin]
+        val id = when (origin) {
+            "african" -> R.string.origin_item_african
+            "albanian" -> R.string.origin_item_albanian
+            "ancientceltic" -> R.string.origin_item_ancient_celtic
+            "ancientegyptian" -> R.string.origin_item_ancient_egyptian
+            "ancientgermanic" -> R.string.origin_item_ancient_germanic
+            "ancientscandinavian" -> R.string.origin_item_ancient_scandinavian
+            "arabic" -> R.string.origin_item_arabic
+            "armenian" -> R.string.origin_item_armenian
+            "astronomy" -> R.string.origin_item_astronomy
+            "basque" -> R.string.origin_item_basque
+            "biblical" -> R.string.origin_item_biblical
+            "breton" -> R.string.origin_item_breton
+            "bulgarian" -> R.string.origin_item_bulgarian
+            "catalan" -> R.string.origin_item_catalan
+            "celtic" -> R.string.origin_item_celtic
+            "chinese" -> R.string.origin_item_chinese
+            "cornish" -> R.string.origin_item_cornish
+            "croatian" -> R.string.origin_item_croatian
+            "czech" -> R.string.origin_item_czech
+            "danish" -> R.string.origin_item_danish
+            "dutch" -> R.string.origin_item_dutch
+            "egyptian" -> R.string.origin_item_egyptian
+            "english" -> R.string.origin_item_english
+            "esperanto" -> R.string.origin_item_esperanto
+            "estonian" -> R.string.origin_item_estonian
+            "fareastern" -> R.string.origin_item_far_eastern
+            "finnish" -> R.string.origin_item_finnish
+            "french" -> R.string.origin_item_french
+            "frisian" -> R.string.origin_item_frisian
+            "galician" -> R.string.origin_item_galician
+            "german" -> R.string.origin_item_german
+            "germanic" -> R.string.origin_item_germanic
+            "greek" -> R.string.origin_item_greek
+            "hawaiian" -> R.string.origin_item_hawaiian
+            "hindu" -> R.string.origin_item_hindu
+            "history" -> R.string.origin_item_history
+            "hungarian" -> R.string.origin_item_hungarian
+            "icelandic" -> R.string.origin_item_icelandic
+            "indian" -> R.string.origin_item_indian
+            "iranian" -> R.string.origin_item_iranian
+            "irish" -> R.string.origin_item_irish
+            "italian" -> R.string.origin_item_italian
+            "japanese" -> R.string.origin_item_japanese
+            "jewish" -> R.string.origin_item_jewish
+            "judeo-christianlegend" -> R.string.origin_item_juedeo_christian_legend
+            "khmer" -> R.string.origin_item_khmer
+            "korean" -> R.string.origin_item_korean
+            "latvian" -> R.string.origin_item_latvian
+            "literature" -> R.string.origin_item_literature
+            "lithuanian" -> R.string.origin_item_lithuanian
+            "macedonian" -> R.string.origin_item_macedonian
+            "manx" -> R.string.origin_item_manx
+            "maori" -> R.string.origin_item_maori
+            "mormon" -> R.string.origin_item_mormon
+            "mythology" -> R.string.origin_item_mythology
+            "nativeamerican" -> R.string.origin_item_native_american
+            "neareastern" -> R.string.origin_item_near_eastern
+            "newworld" -> R.string.origin_item_new_world
+            "norse" -> R.string.origin_item_norse
+            "norwegian" -> R.string.origin_item_norwegian
+            "polish" -> R.string.origin_item_polish
+            "portuguese" -> R.string.origin_item_portuguese
+            "provençal" -> R.string.origin_item_provençal
+            "roman" -> R.string.origin_item_roman
+            "romanian" -> R.string.origin_item_romanian
+            "russian" -> R.string.origin_item_russian
+            "scandinavian" -> R.string.origin_item_scandinavian
+            "scottish" -> R.string.origin_item_scottish
+            "serbian" -> R.string.origin_item_serbian
+            "slavic" -> R.string.origin_item_slavic
+            "slovak" -> R.string.origin_item_slovak
+            "slovene" -> R.string.origin_item_slovene
+            "spanish" -> R.string.origin_item_spanish
+            "swedish" -> R.string.origin_item_swedish
+            "thai" -> R.string.origin_item_thai
+            "theology" -> R.string.origin_item_theology
+            "turkish" -> R.string.origin_item_turkish
+            "ukrainian" -> R.string.origin_item_ukrainian
+            "vietnamese" -> R.string.origin_item_vietnamese
+            "welsh" -> R.string.origin_item_welsh
+            else -> null
+        }
+
         if (id != null) {
             return ctx.getString(id)
         }
